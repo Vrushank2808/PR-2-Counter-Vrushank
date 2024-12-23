18fe08e247c7e0045dd369c2d302147ab8e243f9
Vrushank2808 <vrushankgotawala@gmail.com>
2024-12-23 11:27:46 +0530

    fixed local storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,26 @@ function App() {
   useEffect(() => {
     const savedCount = localStorage.getItem("counterValue");
     if (savedCount !== null) {
-      setCount(JSON.parse(savedCount)); 
+      setCount(JSON.parse(savedCount));
     }
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem("counterValue", JSON.stringify(count));
-    console.log("Stored count:", count);
-  }, [count]);
+  let increment = () => {
+    setCount(count + 1);
+    localStorage.setItem("counterValue", JSON.stringify(count + 1));
+  };
+  let decrement = () => {
+    setCount(count - 1);
+    localStorage.setItem("counterValue", JSON.stringify(count - 1));
+  };
 
   return (
     <>
       <h1>Count: {count}</h1>
       <div className="counter">
-        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={decrement}>-</button>
         <h2>{count}</h2>
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={increment}>+</button>
       </div>
       <button onClick={() => setCount(0)}>Reset</button>
     </>
